c1058fbf1db512e5e2e6f23296da4778fd52c692
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-06-27 14:41:44 -0400

    added main.py and installed/imported pygame and others added some pseudocode for the outline of the game

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame, os, random, time
+pygame.font.init()
+
+# set a window
+
+# set border 
+# set FPS and Vel for bullets and player movement
+# set W adn H for player size
+
+# Import pictures and set colors
+# player 1
+# player 2
+# Bullet
+# Background
+# color dict
+
+## Battlemap gameplay ##
+
+# player class
+    # def to shoot
+    # def to move bullets
+    # def for being hit 
+    # def to draw
+    # def for healthbar
+
+# class for bullet
+    # def for moving
+    # def for draw
+    # def for collision
+    # def for offscreen
+
+# def to draw the winner text
+
+## This is the process for filtering multiple screens ###
+    # def for main menu
+    #     filter clicks to launch different screens
+    #         closing the app
+    #         directions
+    #         controls
+    #         running the gameboard
+
+    # def for gameboard
+    #     filter clicks to launch different screens
+    #         closing to the main menu 
+    #         opening the battle map
+
+    # def for battlemap
+    # define instance of player for player 1 
+    # define instance of player for player 2
+    #     filter for the end of game or quit that returns to game board 
+    #         game ends or close launches gameboard
+
+            
+# call main menu to run the app 
