1eaaded0114c5062ed50db5f1e9dede74f5128f6
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-06-30 16:51:13 -0400

    refactored the sructure of the game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import pygame, os, random, time
 pygame.font.init()
 
@@ -212,158 +213,149 @@ class Structure:
         """checks to see in a structure was hit"""
         return fired_round(self,round)
 
-def draw_main_menu(input):
+def draw_main_menu():
     """draws the main menu and runs the commands"""
-    title = main_font.render(input['title'], 1, color_dict['white'])
-    prompt = main_font.render(input['prompt'], 1, color_dict['white'])
+    WIN.blit(MAIN_MENU_BG, (0,0))
+    title = main_font.render(text_dict['1']['title'], 1, color_dict['white'])
+    prompt = main_font.render(text_dict['1']['prompt'], 1, color_dict['white'])
     WIN.blit(title, (WIDTH/2 - title.get_width()/2,10))
     WIN.blit(prompt, (WIDTH/2 - prompt.get_width()/2 ,HEIGHT/2))
     WIN.blit(UNIT1_IMG, (WIDTH- (WIDTH/4),HEIGHT-200))
     WIN.blit(UNIT2_IMG, (WIDTH/6,HEIGHT-200))
 
-def draw_gameboard(input):
+def draw_gameboard():
     """"starts the gamboard commands"""
-    main_text = main_font.render(input['title'], 1, (255,255,255))
-    WIN.blit(main_text, (10,10))
+    run=True
+    while run:
+        keys = pygame.key.get_pressed()
+        WIN.blit(GAME_BOARD_BG, (0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+                run = False
+            if keys[pygame.K_6]:
+                draw_battlemap(text_dict['3'])
+            pygame.display.update()
 
-def draw_battlemap(input, yellow_player, blue_player, player_vel):
+
+def draw_battlemap(input):
     """starts the battlemap commands"""
-
-    def draw_winner(text):
-        """handles player winning"""
-        draw_text = WINNER_FONT.render(text, 1, color_dict['white'])
-        WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() / 2, HEIGHT /2 - draw_text.get_height() /2 ))
-        pygame.display.update()
-        pygame.time.delay(5000)
-        pygame.QUIT
-
-    keys = pygame.key.get_pressed()
-
-    # handles the player movement and limits the players movement to their perspective boxes
-    if keys[pygame.K_a] and yellow_player.x + player_vel > 0:
-        yellow_player.move('left', player_vel)
-    if keys[pygame.K_d] and yellow_player.x + player_vel + PLAYER_W < WIDTH / 2 - BARB_WIRE.get_width() / 2:
-        yellow_player.move('right', player_vel)
-    if keys[pygame.K_w] and yellow_player.y > 20:
-        yellow_player.move('up', player_vel)
-    if keys[pygame.K_s] and yellow_player.y + player_vel + PLAYER_H < HEIGHT :
-        yellow_player.move('down', player_vel)
-    if keys[pygame.K_LEFT] and blue_player.x + player_vel + PLAYER_W > WIDTH / 2 + BARB_WIRE.get_width() + PLAYER_W:
-        blue_player.move('left', player_vel)
-    if keys[pygame.K_RIGHT] and blue_player.x + player_vel < WIDTH - PLAYER_W:
-        blue_player.move('right', player_vel)
-    if keys[pygame.K_UP] and blue_player.y > 20:
-        blue_player.move('up', player_vel)
-    if keys[pygame.K_DOWN]and blue_player.y + player_vel + PLAYER_H < HEIGHT:
-        blue_player.move('down', player_vel)
-    if keys[pygame.K_v]:
-        yellow_player.build()
-    if keys[pygame.K_n]:
-        blue_player.build(True)
-    if keys[pygame.K_c]:
-        yellow_player.shoot(True)
-    if keys[pygame.K_m]:
-        blue_player.shoot()
-    
-    # checks to see if a bullet hits a player or structure
-    for bullet in yellow_player.bullets:
-        if blue_player.hit(bullet):
-            blue_player.health -= 10 
-            yellow_player.bullets.remove(bullet)
-        for build in blue_player.cover_built:
-            if build.hit(bullet) and build.health > 0:
-                build.health -= 10 
-                yellow_player.bullets.remove(bullet)
-            elif build.health <= 0:
-                blue_player.cover_built.remove(build)
-    for bullet in blue_player.bullets:
-        if yellow_player.hit(bullet):
-            yellow_player.health -= 10 
-            blue_player.bullets.remove(bullet)
-        for build in yellow_player.cover_built:
-            if build.hit(bullet) and build.health > 0:
-                build.health -= 10 
-                blue_player.bullets.remove(bullet)
-            elif build.health <= 0:
-                yellow_player.cover_built.remove(build)
-
-    # draws the barbed wire/ sandbags/ ammo_count
-    WIN.blit(BARB_WIRE, (WIDTH/2 - BARB_WIRE.get_width()/2, 10))
-    for key in input:
-        # draws yellows info
-        if key == 'yellow':
-           yellow_ammo_count = ammo_count_font.render(str(yellow_player.mag),1, color_dict['yellow'])
-           WIN.blit(yellow_ammo_count, (WIDTH - WIDTH/2 - yellow_ammo_count.get_width() - 50,10))
-           for i in range(0, 3-len(yellow_player.cover_built)):
-               WIN.blit(SAND_BAGS,(SAND_BAGS.get_width() * i, 10))
-
-        # draws blues info
-        if key == 'blue':
-            blue_ammo_count = ammo_count_font.render(str(blue_player.mag),1,color_dict['blue'])
-            WIN.blit(blue_ammo_count, (WIDTH/2 + blue_ammo_count.get_width(), 10))
-            for i in range(0, 3-len(blue_player.cover_built)):
-                WIN.blit(SAND_BAGS,(WIDTH - (SAND_BAGS.get_width() * (i+1)), 10))
-
-    # draws the players on the screen
-    yellow_player.draw(WIN)
-    blue_player.draw(WIN)
-
-    # update winner
+  
+    yellow_player = Player(50, HEIGHT - (YELLOW_PLAYER_IMG.get_height() + 50) ,YELLOW_PLAYER_IMG , 'yellow')
+    blue_player = Player(WIDTH - (BLUE_PLAYER_IMG.get_width()+50),100,BLUE_PLAYER_IMG, 'blue')
+    player_vel = 6
     winner_text = ''
-    if yellow_player.health == 0:
-        winner_text = 'Blue player wins!!'
-    if blue_player.health == 0:
-        winner_text = 'Yellow player wins!!'
-    if winner_text != '':
-        draw_winner(winner_text)
+    run = True
 
-    pygame.display.update()
+    while winner_text == '' and run:
+        WIN.blit(BATTLEMAP_BG,(0,0))
+        def draw_winner(text):
+            """handles player winning"""
+            draw_text = WINNER_FONT.render(text, 1, color_dict['white'])
+            WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() / 2, HEIGHT /2 - draw_text.get_height() /2 ))
+            pygame.display.update()
+            pygame.time.delay(5000)
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+                run = False
+        # handles the player movement and limits the players movement to their perspective boxes
+        if keys[pygame.K_a] and yellow_player.x + player_vel > 0:
+            yellow_player.move('left', player_vel)
+        if keys[pygame.K_d] and yellow_player.x + player_vel + PLAYER_W < WIDTH / 2 - BARB_WIRE.get_width() / 2:
+            yellow_player.move('right', player_vel)
+        if keys[pygame.K_w] and yellow_player.y > 20:
+            yellow_player.move('up', player_vel)
+        if keys[pygame.K_s] and yellow_player.y + player_vel + PLAYER_H < HEIGHT :
+            yellow_player.move('down', player_vel)
+        if keys[pygame.K_LEFT] and blue_player.x + player_vel + PLAYER_W > WIDTH / 2 + BARB_WIRE.get_width() + PLAYER_W:
+            blue_player.move('left', player_vel)
+        if keys[pygame.K_RIGHT] and blue_player.x + player_vel < WIDTH - PLAYER_W:
+            blue_player.move('right', player_vel)
+        if keys[pygame.K_UP] and blue_player.y > 20:
+            blue_player.move('up', player_vel)
+        if keys[pygame.K_DOWN]and blue_player.y + player_vel + PLAYER_H < HEIGHT:
+            blue_player.move('down', player_vel)
+        if keys[pygame.K_v]:
+            yellow_player.build()
+        if keys[pygame.K_n]:
+            blue_player.build(True)
+        if keys[pygame.K_c]:
+            yellow_player.shoot(True)
+        if keys[pygame.K_m]:
+            blue_player.shoot()
+        
+        # checks to see if a bullet hits a player or structure
+        for bullet in yellow_player.bullets:
+            if blue_player.hit(bullet):
+                blue_player.health -= 10 
+                yellow_player.bullets.remove(bullet)
+            for build in blue_player.cover_built:
+                if build.hit(bullet) and build.health > 0:
+                    build.health -= 10 
+                    yellow_player.bullets.remove(bullet)
+                elif build.health <= 0:
+                    blue_player.cover_built.remove(build)
+        for bullet in blue_player.bullets:
+            if yellow_player.hit(bullet):
+                yellow_player.health -= 10 
+                blue_player.bullets.remove(bullet)
+            for build in yellow_player.cover_built:
+                if build.hit(bullet) and build.health > 0:
+                    build.health -= 10 
+                    blue_player.bullets.remove(bullet)
+                elif build.health <= 0:
+                    yellow_player.cover_built.remove(build)
 
-def draw_window(background, curr_index, yellow, blue, player_vel,):
-    """This method updates the window"""
-    WIN.blit(background, (0,0))
+        # draws the barbed wire/ sandbags/ ammo_count
+        WIN.blit(BARB_WIRE, (WIDTH/2 - BARB_WIRE.get_width()/2, 10))
+        for key in input:
+            # draws yellows info
+            if key == 'yellow':
+                yellow_ammo_count = ammo_count_font.render(str(yellow_player.mag),1, color_dict['yellow'])
+                WIN.blit(yellow_ammo_count, (WIDTH - WIDTH/2 - yellow_ammo_count.get_width() - 50,10))
+                for i in range(0, 3-len(yellow_player.cover_built)):
+                    WIN.blit(SAND_BAGS,(SAND_BAGS.get_width() * i, 10))
 
-    # filter what to display 
-    if curr_index == 1:
-        draw_main_menu(text_dict['1'])
-    if curr_index == 2:
-        draw_gameboard(text_dict['2'])
-    if curr_index == 3:
-        draw_battlemap(text_dict['3'], yellow, blue, player_vel)
+            # draws blues info
+            if key == 'blue':
+                blue_ammo_count = ammo_count_font.render(str(blue_player.mag),1,color_dict['blue'])
+                WIN.blit(blue_ammo_count, (WIDTH/2 + blue_ammo_count.get_width(), 10))
+                for i in range(0, 3-len(blue_player.cover_built)):
+                    WIN.blit(SAND_BAGS,(WIDTH - (SAND_BAGS.get_width() * (i+1)), 10))
 
-    pygame.display.update()
+        # draws the players on the screen
+        yellow_player.draw(WIN)
+        blue_player.draw(WIN)
+
+        # update winner
+        if yellow_player.health == 0:
+            winner_text = 'Blue player wins!!'
+        if blue_player.health == 0:
+            winner_text = 'Yellow player wins!!'
+        if winner_text != '':
+            draw_winner(winner_text)
+
+        pygame.display.update()
 
 ### This is the process for filtering multiple screens ###
 def main():
-    """This will handle the main events of the game"""
-    # initial defining
+    """This is the main menu handling initial game events"""
     clock = pygame.time.Clock()
-    yellow_player = Player(50, HEIGHT - (YELLOW_PLAYER_IMG.get_height() + 50) ,YELLOW_PLAYER_IMG , 'yellow')
-    blue_player = Player(WIDTH - (BLUE_PLAYER_IMG.get_width()+50),100,BLUE_PLAYER_IMG, 'blue')
-    current_bg_index = 1
-    player_vel = 6
     run = True
+    draw_main_menu()
 
     while run:
         # draw main menu background
         clock.tick(FPS)
-        draw_window(BG_DICT[str(current_bg_index)], current_bg_index, yellow_player, blue_player, player_vel)
         # default quit function in loop
         for event in pygame.event.get():
-            if event.type == YELLOW_HIT:
-                yellow_player.health -= 10
-            if event.type == BLUE_HIT:
-                blue_player.health -= 10
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE] or event.type == pygame.QUIT:
                 run = False
             if keys[pygame.K_SPACE]:
-                current_bg_index = 2
-            if keys[pygame.K_RETURN]:
-                current_bg_index = 3
-                # draw_battlemap(BG_DICT['3'], yellow_player, blue_player, player_vel)
-            if keys[pygame.K_y]:
-                current_bg_index = 1
+                draw_gameboard()
+
+        pygame.display.update()
 
 # only runs the game if the file name is "main"
 if __name__ == '__main__':
